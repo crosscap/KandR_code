8df9259c7649873e47b00063bc87d494be321b63
crosscap wang <wangliankun2018@outlook.com>
2022-08-28 18:12:14 +0800

    20220828-1

---
+++ e3/e3-2.c
@@ -0,0 +1,75 @@
+void escape(char s[], char t[])
+{
+    int i, j;
+
+    i = j = 0;
+    while (t[i])
+    {
+        switch (t[i])
+        {
+        case '\n':
+            s[j++] = '\\';
+            s[j++] = 'n';
+            ++i;
+            break;
+        case '\t':
+            s[j++] = '\\';
+            s[j++] = 't';
+            ++i;
+            break;
+        case '\b':
+            s[j++] = '\\';
+            s[j++] = 'b';
+            ++i;
+            break;
+        case '\\':
+            s[j++] = '\\';
+            s[j++] = '\\';
+            ++i;
+            break;
+        default:
+            s[j++] = t[i++];
+            break;
+        }
+    }
+}
+
+void reescape(char s[], char t[])
+{
+    int i, j;
+
+    i = j = 0;
+    while (t[i])
+    {
+        switch (t[i])
+        {
+        case '\\':
+            switch (t[i+1])
+            {
+                case 'n':
+                    s[j++] = '\n';
+                    i += 2;
+                    break;
+                case 't':
+                    s[j++] = '\t';
+                    i += 2;
+                    break;
+                case 'b':
+                    s[j++] = '\b';
+                    i += 2;
+                    break;
+                case '\\':
+                    s[j++] = '\\';
+                    i += 2;
+                    break;
+                default:
+                    i += 2;
+                    break;
+            }
+            break;
+        default:
+            s[j++] = t[i++];
+            break;
+        }
+    }
+}
